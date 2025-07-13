628e698aa3b3d4dc8feafc37fde4ff2c978a8e4f
Thambolo <84055721+Thambolo@users.noreply.github.com>
2025-07-13 10:24:42 +1000

    make collab popup dynamic

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import react from '@vitejs/plugin-react-swc';
+import {componentTagger} from 'lovable-tagger';
+import path from 'path';
+import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    proxy: {
-      '/api': 'http://localhost:3001',
-    },
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig(({mode}) => ({
+                              server: {
+                                host: '::',
+                                port: 8080,
+                                proxy: {
+                                  '/api': 'http://localhost:3001',
+                                },
+                              },
+                              plugins: [
+                                react(),
+                                mode === 'development' && componentTagger(),
+                              ].filter(Boolean),
+                              resolve: {
+                                alias: {
+                                  '@': path.resolve(__dirname, './src'),
+                                },
+                              },
+                            }));
